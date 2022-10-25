bc352c1bc5eda2b6fb207c76f2da7cff40edf8b2
s217481094 <s217481094@R227-10>
2022-10-25 13:46:22 +0200

    patient

--- Areas/Patient/Controllers/HomeController.cs
+++ Areas/Patient/Controllers/HomeController.cs
@@ -1,17 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_prescription.Models;
+using E_prescription.Models.Account;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace E_prescription.Areas.Patient.Controllers
 {
+    [Area("Patient")]
     public class HomeController : Controller
     {
-        [Area("Patient")]
+        private readonly GRP42EPrescriptionContext _context = new GRP42EPrescriptionContext();
+
+        private readonly IConfiguration configuration;
+        DataAccess data;
+        public HomeController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+       //[HttpPost]
+        //public IActionResult PrescriptionList()
+        //{
+          
+        //}
     }
 }
---
+++ Areas/Patient/Models/PrescriptionListModel.cs
@@ -0,0 +1,25 @@
+﻿using E_prescription.Models;
+using System;
+using System.Collections.Generic;
+
+namespace E_prescription.Areas.Patient.Models
+{
+    public class PrescriptionListModel
+    {
+     
+        public int PatientId { get; set; }
+        public string Idnumber { get; set; }
+        public string Dob { get; set; }
+        public DateTime? Date { get; set; }
+        public int PrescriptionId { get; set; }
+        public int? DoctorId { get; set; }
+
+        public virtual TblUser PatientNavigation { get; set; }
+        public virtual ICollection<ChronicHistory> ChronicHistories { get; set; }
+        public virtual ICollection<ChronicMedication> ChronicMedications { get; set; }
+        public virtual ICollection<DispensedMedication> DispensedMedications { get; set; }
+        public virtual ICollection<PatientAllergy> PatientAllergies { get; set; }
+        public virtual ICollection<PatientDoctorVisit> PatientDoctorVisits { get; set; }
+        public virtual ICollection<Prescription> Prescriptions { get; set; }
+    }
+}
